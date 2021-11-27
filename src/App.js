295841288aff545407cabd5586a1fcac700c4aed
Chris <neoxrock@gmail.com>
2021-11-27 23:50:43 +1100

    api async test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import CounterContainer from "./containers/CounterContainer";
+import SampleContainer from "./containers/SampleContainer";
 
 function App() {
   return (
     <div>
       <CounterContainer></CounterContainer>
+      <SampleContainer></SampleContainer>
     </div>
   );
 }
