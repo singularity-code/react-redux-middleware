e9cd591bb749dc1189e2a7588dd7deae26ca0adb
DL <neoxrock@gmail.com>
2021-11-28 15:08:08 +1100

    refactor request

--- src/modules/sample.js
+++ src/modules/sample.js
@@ -1,7 +1,6 @@
-import {
-  handleActions
-} from 'redux-actions';
-import * as api from '../lib/api'
+import { handleActions } from 'redux-actions';
+import * as api from '../lib/api';
+import createRequestThunk from '../lib/createRequestThunk';
 
 //actions
 const GET_POST = 'sample/GET_POST';
@@ -14,93 +13,59 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 //thunk functions
 
-export const getPost = id => async dispatch => {
-  dispatch({
-    type: GET_POST
-  })
-  try {
-    const response = await api.getPost(id);
-    dispatch({
-      type: GET_POST_SUCCESS,
-      payload: response.data
-    })
-  } catch (e) {
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true
-    })
-    throw e;
-  }
-}
-
-export const getUsers = id => async dispatch => {
-  dispatch({
-    type: GET_USERS
-  })
-  try {
-    const response = await api.getUsers(id);
-    dispatch({
-      type: GET_POST_SUCCESS,
-      payload: response.data
-    })
-  } catch (e) {
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true
-    })
-    throw e;
-  }
-}
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
 
 const initialState = {
   loading: {
     GET_POST: false,
-    GET_USERS: false
+    GET_USERS: false,
   },
   post: null,
-  users: null
-}
+  users: null,
+};
 
-const sample = handleActions({
-  [GET_POST]: state => ({
-    ...state,
-    loading: {
-      ...state.loading,
-      GET_POST: true
-    }
-  }),
-  [GET_POST_SUCCESS]: (state, action) => ({
-    ...state,
-    loading: {
-      ...state.loading,
-      GET_POST: false
-    },
-    post: action.payload
-  }),
-  [GET_POST_FAILURE]: (state, action) => ({
-    ...state,
-    loading: {
-      ...state.loading,
-      GET_USERS: true
-    },
-  }),
-  [GET_USERS_SUCCESS]: (state, action) => ({
-    ...state,
-    loading: {
-      ...state.loading,
-      GET_USERS: false
-    },
-    users: action.payload
-  }),
-  [GET_USERS_FAILURE]: (state, action) => ({
-    ...state,
-    loading: {
-      ...state.loading,
-      GET_USERS: false
-    },
-  })
-}, initialState);
+const sample = handleActions(
+  {
+    [GET_POST]: (state) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_POST: true,
+      },
+    }),
+    [GET_POST_SUCCESS]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_POST: false,
+      },
+      post: action.payload,
+    }),
+    [GET_POST_FAILURE]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_USERS: true,
+      },
+    }),
+    [GET_USERS_SUCCESS]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_USERS: false,
+      },
+      users: action.payload,
+    }),
+    [GET_USERS_FAILURE]: (state, action) => ({
+      ...state,
+      loading: {
+        ...state.loading,
+        GET_USERS: false,
+      },
+    }),
+  },
+  initialState,
+);
 
 export default sample;
